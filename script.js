586a96381d97cde2f812a67de7fee7444a2b6c2b
Mustafa <30720101+akbasmustafa@users.noreply.github.com>
2021-03-14 16:31:07 +0000

    Completed Level 100 design

--- script.js
+++ script.js
@@ -6,7 +6,31 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  //Create a Card for each Episode Object in episodeList Array
+  episodeList.forEach((episode) => {
+    const card = document.createElement("div");
+    card.className = "card-style";
+
+    const headerText = document.createElement("h5");
+    let text = episode.name + " - S" + episode.season + "E" + episode.number;
+    headerText.innerHTML = text;
+    headerText.className = "card-header";
+    card.appendChild(headerText);
+
+    const image = document.createElement("img");
+    image.src = episode.image.medium;
+    image.className = "card-image";
+    card.appendChild(image);
+
+    const summaryText = document.createElement("p");
+    summaryText.innerHTML = episode.summary;
+    summaryText.className = "card-summary";
+    card.appendChild(summaryText);
+
+    rootElem.appendChild(card);
+  });
 }
 
 window.onload = setup;
