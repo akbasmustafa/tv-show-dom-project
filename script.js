2b97d2b80ae134dc55fc427fc6d3e06a612aeb27
Mustafa <30720101+akbasmustafa@users.noreply.github.com>
2021-03-15 23:38:12 +0000

    minor fixed

--- script.js
+++ script.js
@@ -9,23 +9,28 @@ function makePageForEpisodes(episodeList) {
   //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
   //Create a Card for each Episode Object in episodeList Array
-  episodeList.forEach((episode) => {
+  episodeList.forEach(({ name, season, number, summary, image }) => {
     const card = document.createElement("div");
     card.className = "card-style";
 
     const headerText = document.createElement("h5");
-    let text = episode.name + " - S" + episode.season + "E" + episode.number;
+    let text =
+      name +
+      " - S" +
+      season.toString().padStart(2, "0") +
+      "E" +
+      number.toString().padStart(2, "0");
     headerText.innerHTML = text;
     headerText.className = "card-header";
     card.appendChild(headerText);
 
-    const image = document.createElement("img");
-    image.src = episode.image.medium;
-    image.className = "card-image";
-    card.appendChild(image);
+    const img = document.createElement("img");
+    img.src = image.medium;
+    img.className = "card-image";
+    card.appendChild(img);
 
     const summaryText = document.createElement("p");
-    summaryText.innerHTML = episode.summary;
+    summaryText.innerHTML = summary;
     summaryText.className = "card-summary";
     card.appendChild(summaryText);
 
